71c9e1274422f10762ca6e2e84ea9e7ac234efae
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-06-17 02:40:21 -0300

    -q

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    height: '100vh',
+    justify: 'center',
+    align: 'center',
+    wrap: 'wrap',
+    wrap2: 'wrap2',
+    direction: 'row',
+    basis: '100',
+    grow: '0',
+    counter: '1',
+
+    newItem: '',
+    items: [1, 2]
+  },
+   methods: {
+    addNewItem: function () {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+     reset: function () {
+      this.items = [1, 2],
+      this.grow = '0'
+      this.basis = '100'
+    }
+  }
+})
